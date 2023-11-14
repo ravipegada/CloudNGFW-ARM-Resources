09523dfc0c577020e93e4b863204d231d2e74c7e
ravipegada <100585753+ravipegada@users.noreply.github.com>
2023-11-14 22:10:55 +0530

    Create WebServerInit.sh

---
+++ WebServerInit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# package updates
+sudo apt update 
+# apache installation, enabling and status check
+sudo apt -y install apache2
+# Copy files to showcase security use case
+sudo wget -P /var/www/html/  https://github.com/ravipegada/CloudNGFW-ARM-Resources/blob/main/web-to-db-attack.html
+sudo wget -p /var/www/html/  https://github.com/ravipegada/CloudNGFW-ARM-Resources/blob/main/sql-attack.html
+sudo wget -p /var/www/html/  https://github.com/ravipegada/CloudNGFW-ARM-Resources/blob/main/showheaders.php
+sudo wget -p /var/www/html/  https://github.com/ravipegada/CloudNGFW-ARM-Resources/blob/main/index.html
+sudo wget -p /usr/lib/cgi-bin/ https://github.com/ravipegada/CloudNGFW-ARM-Resources/blob/main/guess-sql-root-password.cgi
+sudo service apache2 restart
