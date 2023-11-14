aac4e8f6b1d357ccca978cb1be2a1e3a860c0357
ravipegada <100585753+ravipegada@users.noreply.github.com>
2023-11-14 23:18:47 +0530

    Update WebServerInit.sh

--- WebServerInit.sh
+++ WebServerInit.sh
@@ -4,10 +4,10 @@ sudo apt update
 # apache installation, enabling and status check
 sudo apt -y install apache2
 # Copy files to showcase security use case
-sudo wget -P /var/www/html/  https://github.com/ravipegada/CloudNGFW-ARM-Resources/blob/main/web-to-db-attack.html
-sudo wget -P /var/www/html/  https://github.com/ravipegada/CloudNGFW-ARM-Resources/blob/main/sql-attack.html
-sudo wget -P /var/www/html/  https://github.com/ravipegada/CloudNGFW-ARM-Resources/blob/main/showheaders.php
-sudo wget -P /var/www/html/  https://github.com/ravipegada/CloudNGFW-ARM-Resources/blob/main/index.html
-sudo wget -P /usr/lib/cgi-bin/ https://github.com/ravipegada/CloudNGFW-ARM-Resources/blob/main/guess-sql-root-password.cgi
+sudo wget -P /var/www/html/  https://raw.githubusercontent.com/ravipegada/CloudNGFW-ARM-Resources/main/web-to-db-attack.html
+sudo wget -P /var/www/html/  https://raw.githubusercontent.com/ravipegada/CloudNGFW-ARM-Resources/blob/main/sql-attack.html
+sudo wget -P /var/www/html/  https://raw.githubusercontent.com/ravipegada/CloudNGFW-ARM-Resources/blob/main/showheaders.php
+sudo wget -P /var/www/html/  https://raw.githubusercontent.com/ravipegada/CloudNGFW-ARM-Resources/blob/main/index.html
+sudo wget -P /usr/lib/cgi-bin/ https://raw.githubusercontent.com/ravipegada/CloudNGFW-ARM-Resources/blob/main/guess-sql-root-password.cgi
 sudo service apache2 restart
 
