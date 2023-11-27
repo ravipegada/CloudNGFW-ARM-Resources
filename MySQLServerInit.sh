e62939ebea1ad665e751a127fbf0b11180c8a80c
ravipegada <100585753+ravipegada@users.noreply.github.com>
2023-11-27 09:43:07 +0530

    Update MySQLServerInit.sh

--- MySQLServerInit.sh
+++ MySQLServerInit.sh
@@ -3,3 +3,9 @@
 sudo apt update 
 # MySQL Server installation, enabling and status check
 sudo apt -y install mysql-server
+# Create User
+sudo mysql
+CREATE USER 'user1'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON * . * TO 'user1'@'localhost';
+FLUSH PRIVILEGES;
+exit
