c269c570a4c3bff2109d2d824df7bce311546182
ravipegada <100585753+ravipegada@users.noreply.github.com>
2023-11-27 09:39:57 +0530

    Update WebServerInit.sh

--- WebServerInit.sh
+++ WebServerInit.sh
@@ -9,5 +9,6 @@ sudo wget -P /var/www/html/  https://raw.githubusercontent.com/ravipegada/CloudN
 sudo wget -P /var/www/html/  https://raw.githubusercontent.com/ravipegada/CloudNGFW-ARM-Resources/main/showheaders.php
 sudo wget -P /var/www/html/  https://raw.githubusercontent.com/ravipegada/CloudNGFW-ARM-Resources/main/index.html
 sudo wget -P /usr/lib/cgi-bin/ https://raw.githubusercontent.com/ravipegada/CloudNGFW-ARM-Resources/main/guess-sql-root-password.cgi
+sudo chmod 755 /usr/lib/cgi-bin/guess-sql-root-password.cgi
 sudo service apache2 restart
 
